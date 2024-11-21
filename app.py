a839aedc50bcd8789c87d3c2404936870fae258f
Praveen <praveen.kumar@manipalgroup.info>
2024-11-21 10:35:38 +0530

    “commentedblurchecking”

--- app.py
+++ app.py
@@ -33,12 +33,11 @@ def predict():
         image = cv2.imread(image_path)
 
         eye_detected = three_eye_detect(image, EYE_DETECTION)
-
         if eye_detected:
-            is_blur = is_blurry_image(image)
-            if is_blur:
-                rtrn = 4
-            else:
+            # is_blur = is_blurry_image(image)
+            # if is_blur:
+            #     rtrn = 4
+            # else:
                 main_result = detection_tflite(image, MAIN_DETECTION)
                 if len(main_result) >= 1:
                     dot_detect = process_dots(image, DOT_DETECTION)
